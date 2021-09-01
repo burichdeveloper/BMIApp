10ab40636a027e36ac217e7b028518e784c639f0
Rod Burich <burich.developer@gmail.com>
2021-09-01 00:52:39 -0600

    Upload

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Rodrigo Dominguez Burich on 2020-01-31.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
